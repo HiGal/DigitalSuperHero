784ef35a60a993c7184c19ddbf641272d292d4c4
Ruslan Shakirov <shakirovrrr@outlook.com>
2019-04-06 16:00:16 +0300

    UserCustomer

--- Models/UserCompany.py
+++ Models/UserCompany.py
@@ -14,16 +14,13 @@ def db_connect():
     return psycopg2.connect(dbname=dbname, user=user, password=password, host=host)
 
 
-class UserCustomer:
-    def __init__(self, email: str = None, password: str = None, phone: str = None, name: str = None,
-                 surname: str = None, midname: str = None):
+class UserCompany:
+    def __init__(self, email: str = None, password: str = None, company: str = None, INN: str = None):
         self.conn = db_connect()
         self.email: str = email
         self.password: str = password if password is None else hash_password(password)
-        self.phone: str = phone
-        self.name: str = name
-        self.surname: str = surname
-        self.midname: str = midname
+        self.company: str = company
+        self.INN: str = INN
         self.verified: bool = False
 
     def verify(self) -> bool:
@@ -31,10 +28,9 @@ class UserCustomer:
             return True
 
         cursor = self.conn.cursor()
-        password = hash_password(self.password)
         cursor.execute(
-            'SELECT * FROM user_customer WHERE email = \'{}\' AND password = \'{}\''
-                .format(self.email, password)
+            'SELECT * FROM user_company WHERE email = \'{}\' AND password = \'{}\''
+                .format(self.email, self.password)
         )
 
         count = cursor.rowcount
@@ -47,21 +43,19 @@ class UserCustomer:
         self.verified = True
         return True
 
-    def register(self, email: str, password: str, phone: str, name: str, surname: str, midname: str):
+    def register(self, email: str, password: str, company: str, inn: str):
         self.email: str = email
         self.password: str = password
-        self.phone: str = phone
-        self.name: str = name
-        self.surname: str = surname
-        self.midname: str = midname
+        self.company: str = company
+        self.INN: str = inn
 
         h_pass = hash_password(password)
 
         cursor = self.conn.cursor()
         cursor.execute(
-            'INSERT INTO user_customer (email, password, phone, name, surname, midname) '
-            'VALUES (\'{}\',\'{}\',\'{}\',\'{}\',\'{}\',\'{}\')'
-                .format(email, h_pass, phone, name, surname, midname)
+            'INSERT INTO user_company (email, password, company_name, nalog_id) '
+            'VALUES (\'{}\',\'{}\',\'{}\',\'{}\')'
+                .format(email, h_pass, company, inn)
         )
         self.conn.commit()
         cursor.close()
@@ -71,7 +65,7 @@ class UserCustomer:
     def retrieve(self) -> bool:
         cursor = self.conn.cursor()
         cursor.execute(
-            'SELECT * FROM user_customer '
+            'SELECT * FROM user_company '
             'WHERE email = \'{}\''.format(self.email)
         )
 
@@ -82,9 +76,7 @@ class UserCustomer:
         record = next(cursor)
         cursor.close()
 
-        self.phone = record[2]
-        self.name = record[3]
-        self.surname = record[4]
-        self.midname = record[5]
+        self.company = record[2]
+        self.INN = record[3]
 
         return True
