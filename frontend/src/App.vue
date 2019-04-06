2cc350b0ae29f803e0254aeb6d393551ee061a0d
HiGal <f.galeev@innopolis.ru>
2019-04-06 18:07:28 +0300

    Commit styles for front

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,22 +1,33 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/login">Login</router-link> |
-      <router-link to="/registration_contractor">Registration_contractor</router-link> |
-      <router-link to="/registration_customer">Registration_customer</router-link>
+    <div id="app">
+        <b-navbar toggleable="lg" type="dark" variant="info">
+            <b-navbar-brand href="#">Сетевая компания</b-navbar-brand>
+            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+            <b-collapse id="nav-collapse" is-nav>
+                <b-navbar-nav  >
+                    <b-nav-item  href="#">
+                        <router-link style="color: white" to="/">Home</router-link>
+                    </b-nav-item>
+                    <b-nav-item href="#">
+                        <router-link style="color: white" to="/login">Login</router-link>
+                    </b-nav-item>
+                    <b-nav-item href="#">
+                        <router-link style="color: white" to="/registration_contractor">Registration_contractor</router-link>
+                    </b-nav-item>
+                    <b-nav-item href="#">
+                        <router-link style="color: white" to="/registration_customer">Registration_customer</router-link>
+                    </b-nav-item>
+
+                </b-navbar-nav>
+
+
+            </b-collapse>
+        </b-navbar>
+        <router-view/>
     </div>
-    <router-view />
-  </div>
 </template>
 
 <style lang="css">
-  @import "assets/css/style.css";
+    @import "assets/css/style.css";
 </style>
-<script>
-    import Home from "./views/Home";
-    import Kanban from "./components/Kanban";
-    export default {
-        components: {Kanban, Home}
-    }
-</script>
+
