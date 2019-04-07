24eb7e2c5b1451d6d78fd8cb63d2246dc397ab59
Lenar Gumerov <lenargamgam@gmail.com>
2019-04-07 11:48:03 +0300

    my_requests and app.py connection

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ from flask import Flask
 from flask_cors import CORS
 from login import login_page
 from marketplace import marketplace
+from my_requests import my_requests
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 app.register_blueprint(login_page)
 app.register_blueprint(marketplace)
+app.register_blueprint(my_requests)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
