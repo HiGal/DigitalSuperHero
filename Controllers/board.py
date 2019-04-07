75f8a7df0e503e799dd5af8d0784a31f768d16c0
Lenar Gumerov <lenargamgam@gmail.com>
2019-04-07 16:35:05 +0300

    board hotfix

--- Controllers/board.py
+++ Controllers/board.py
@@ -7,6 +7,20 @@ kanban = Blueprint("board", __name__)
 CORS(kanban)
 
 
+def get_board():
+    orders = {}
+    for stage in OrderStage:
+        stage_orders = []
+        for order in Order.get_orders_by_stage(stage):
+            stage_orders.append({"id": str(order.id),
+                                 "task": order.task,
+                                 "description": order.description,
+                                 "email": order.customer_email})
+        if len(stage_orders) != 0:
+            orders.update({stage.stage_name(): stage_orders})
+    return orders
+
+
 @kanban.route("/board", methods=["GET", "POST"])
 def board():
     if request.method == "POST":
@@ -14,56 +28,26 @@ def board():
         if data["action"] == "get":
             user = UserCompany(data["email"], data["password"])
             if user.verify():
-                orders = {}
-                for stage in OrderStage:
-                    stage_orders = {}
-                    for order in Order.get_orders_by_stage(stage):
-                        stage_orders.update({"id": order.id,
-                                             "task": order.task,
-                                             "description": order.description,
-                                             "email": order.customer_email})
-                    orders.update({stage.stage_name(): stage_orders})
-                return jsonify(orders)
+                return jsonify(get_board())
         elif data["action"] == "del":
             order = Order()
             order.retrieve(data["id"])
             order.remove()
-            orders = {}
-            for stage in OrderStage:
-                stage_orders = {}
-                for order in Order.get_orders_by_stage(stage):
-                    stage_orders.update({"id": order.id,
-                                         "task": order.task,
-                                         "description": order.description,
-                                         "email": order.customer_email})
-                orders.update({stage.stage_name(): stage_orders})
-            return jsonify(orders)
+            user = UserCompany(data["email"], data["password"])
+            if user.verify():
+                return jsonify(get_board())
         elif data["action"] == "up":
             order = Order()
             order.retrieve(data["id"])
             order.move_stage_next()
-            orders = {}
-            for stage in OrderStage:
-                stage_orders = {}
-                for order in Order.get_orders_by_stage(stage):
-                    stage_orders.update({"id": order.id,
-                                         "task": order.task,
-                                         "description": order.description,
-                                         "email": order.customer_email})
-                orders.update({stage.stage_name(): stage_orders})
-            return jsonify(orders)
+            user = UserCompany(data["email"], data["password"])
+            if user.verify():
+                return jsonify(get_board())
         elif data["action"] == "down":
             order = Order()
             order.retrieve(data["id"])
             order.move_stage_back()
-            orders = {}
-            for stage in OrderStage:
-                stage_orders = {}
-                for order in Order.get_orders_by_stage(stage):
-                    stage_orders.update({"id": order.id,
-                                         "task": order.task,
-                                         "description": order.description,
-                                         "email": order.customer_email})
-                orders.update({stage.stage_name(): stage_orders})
-            return jsonify(orders)
+            user = UserCompany(data["email"], data["password"])
+            if user.verify():
+                return jsonify(get_board())
     return Response("board.html")
