6d41059923b43888fc01dee7b45e8e528de34923
Lenar Gumerov <lenargamgam@gmail.com>
2019-04-07 11:50:59 +0300

    my_requests update

--- my_requests.py
+++ my_requests.py
@@ -6,11 +6,6 @@ my_requests = Blueprint("my_requests", __name__)
 CORS(my_requests)
 
 
-@my_requests.route("/")
-def hello_world():
-    return "Hello World!"
-
-
 @my_requests.route("/my_requests", methods=["GET", "POST"])
 def my_requests():
     if request.method == "POST":
